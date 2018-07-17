c5f2513f59e97443f825a1eb4a7a8456e1efb911
AmauriAires <amauriairesfilho@gmail.com>
2018-07-17 06:32:43 -0400

    Added the main functionalities of the application

--- vehicleWebApp/Controllers/HomeController.cs
+++ vehicleWebApp/Controllers/HomeController.cs
@@ -4,7 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using vehicleWebApp.Models;
+using System.Net;
+using System.IO;
 
 namespace vehicleWebApp.Controllers
 {
@@ -34,10 +37,32 @@ namespace vehicleWebApp.Controllers
             return View();
         }
 
+        public string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+       
+        [HttpPost]
+        public string FirstAjax(string a)
+        {
+            string response = Get("https://localhost:5003/api/vehicles/Civic/Honda/Blue/2016");
+            string ans = JsonConvert.SerializeObject(response, Formatting.Indented);
+            return ans;
+        }
+
     }
 }
--- vehicleWebApp/wwwroot/js/site.js
+++ vehicleWebApp/wwwroot/js/site.js
@@ -2,3 +2,53 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//$.getJSON( "https://127.0.0.1:5003/api/vehicles/c/c/c/c", function( data ){
+//  console.log( data );
+//});
+
+
+
+function ChangecatList(){
+    //alert($('#validationCustom01').val());
+}
+
+function ChangecatList2(){
+    //alert($('#validationCustom01').val());
+}
+function ChangecatList3(){
+    //alert($('#validationCustom01').val());
+}
+function ChangecatList4(){
+    //alert($('#validationCustom01').val());
+}
+
+
+$('#submitBtn').on("click",function() {
+    var serviceURL = '/Home/FirstAjax';
+    var name = $('#validationCustom01').val();
+    var brand = $('#validationCustom02').val();
+    var color = $('#validationCustom03').val();
+    var year = $('#validationCustom04').val();
+    console.log('name:' + name +', ' + 'brand: ' + brand +', ' + 'color: ' + color +', ' + 'year: ' + year);
+
+    $.ajax({
+        type: "POST",
+        url: serviceURL,
+        data: JSON.stringify({name: name}),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: successFunc,
+        error: errorFunc
+    });
+
+    function successFunc(data, status) { 
+        console.log(data);
+        $(".carousel-inner img").each(function(index,ele){
+            $(this).attr('src','/images/'+JSON.parse(data).Pictures[index]);
+        });
+    }
+
+    function errorFunc() {
+        alert('error');
+    }
+}); 
